22fb1256450b745e11ee07a84a58019d683394b3
Noah Lepage <noah.lepage@mail.mcgill.ca>
2024-01-27 20:05:17 -0500

    canvas

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,22 @@
-import { Box, useMediaQuery, useTheme } from "@mui/material";
+import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
 import React from "react";
+import Canvas from "../components/Canvas";
 
 function Home() {
   const theme = useTheme();
   const isMatch = useMediaQuery(theme.breakpoints.down("md"));
 
-  // Subset of props, to illustrate the idea.
-  const config = isMatch ? { fontSize: 26 } : { fontSize: 30 };
+  const draw = (ctx, frameCount) => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.strokeStyle = "#000";
+    // ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(1000000, 1000000);
+    // ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+    ctx.stroke();
+  };
 
-  // Here only root <Box/> is configured, but you can configure all the nested components the same way.
-  return (
-    <div className="mainPage">
-      <Box sx={config}>[...] teqipwehboiqbenoqw</Box>
-    </div>
-  );
+  return <Canvas draw={draw} />;
 }
 
 export default Home;
