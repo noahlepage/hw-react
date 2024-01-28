8b83fbc45a1309c6ba11399cf42256c39025c72c
Noah Lepage <noah.lepage@mail.mcgill.ca>
2024-01-27 19:30:01 -0500

    adapt to screen width

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
+import { Box, useMediaQuery, useTheme } from "@mui/material";
 import React from "react";
 
 function Home() {
+  const theme = useTheme();
+  const isMatch = useMediaQuery(theme.breakpoints.down("md"));
+
+  // Subset of props, to illustrate the idea.
+  const config = isMatch ? { fontSize: 26 } : { fontSize: 30 };
+
+  // Here only root <Box/> is configured, but you can configure all the nested components the same way.
   return (
-    <div>
-      <h1>Welcome to the Home Page!</h1>
+    <div className="mainPage">
+      <Box sx={config}>[...] teqipwehboiqbenoqw</Box>
     </div>
   );
 }
